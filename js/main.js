07092b57a9ad9433d190226275f97263bb039bed
Pratik <pdfulkar@yahoo.com>
2023-08-09 04:05:11 +0530

    changed readme

--- js/main.js
+++ js/main.js
@@ -64,6 +64,7 @@
 	});
 
 	// Activate scrollspy to add active class to navbar items on scroll
+	
 	$('body').scrollspy({
 		target: '#mainNav',
 		offset: navHeight
@@ -89,29 +90,6 @@
 		}
 	});
 
-	/*--/ Star Typed /--*/
-	if ($('.text-slider').length == 1) {
-    var typed_strings = $('.text-slider-items').text();
-		var typed = new Typed('.text-slider', {
-			strings: typed_strings.split(','),
-			typeSpeed: 80,
-			loop: true,
-			backDelay: 1100,
-			backSpeed: 30
-		});
-	}
 
-	/*--/ Testimonials owl /--*/
-	$('#testimonial-mf').owlCarousel({
-		margin: 20,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-			}
-		}
-	});
 
 })(jQuery);
